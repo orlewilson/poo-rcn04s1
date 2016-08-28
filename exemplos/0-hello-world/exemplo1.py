366c6bec9e7f2ed0e19f27e69c0673e89f38565a
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2016-08-28 18:55:44 -0400

    Exemplos vistos em sala de aula no dia 19/08/2016

---
+++ exemplos/0-hello-world/exemplo1.py
@@ -0,0 +1,84 @@
+"""
+Exemplo de comentário em várias linhas
+"""
+
+# Exemplo de comentário em uma linha
+
+"""
+Disciplina:	Programação Orientada a Objetos
+Professor:	Orlewilson B. Maia
+Data:		19/08/2016
+
+Autor:		Orlewilson B. Maia
+Descrição:	Exemplos de comandos em Python
+"""
+
+# impressão de texto no console
+print("Hello World")
+
+
+# tipos de variáveis
+
+a = 2 
+print(a)
+print(type(a)) # int (inteiro)
+
+a = 2.2 
+print(a)
+print(type(a)) # float (real)
+
+a = "2.2"
+print(a)
+print(type(a)) # string (caracter)
+
+a = 'teste 2'
+print(a)
+print(type(a)) # string (caracter)
+
+a = True
+print(a)
+print(type(a)) # bool (boleano)
+
+a = False
+print(a)
+print(type(a)) # bool (boleano)
+
+a = None
+print(a)
+print(type(a)) # sem tipo 
+
+# Operações
+print (2 + 2) # soma
+
+print (4 - 5) # subtração
+
+print (4 * 5) # multiplicação
+
+print (4 / 5) # divisão (float)
+print (int(4 / 5)) # divisão (int)
+
+print (4 % 5) # resto da divisão
+
+print (3 ** 2) # potenciação
+
+# declaração de variáveis
+
+variavel = 1	# válido
+variavel2 = 1	# válido
+#variavel 3 = 1	# inválido
+#4variavel = 1	# inválido
+_variavel5 = 1	# válido
+v6ariavel = 1	# válido
+
+# entrada de texto
+a = input("Digite algo: ")
+print(a)
+
+# entrada de texto e converte para inteiro
+b = int (input("Digite um número inteiro: "))
+print(b)
+
+# entrada de texto e converte para float
+c = float (input("Digite um número real: "))
+print(c)
+
